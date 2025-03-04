95d75fca3354d1869a7131aa7aa76fe115dae61e
GaxeStore <mygaxestore@gmail.com>
2025-03-04 15:43:56 +0700

    Add files via upload

--- Gaxee.py
+++ Gaxee.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import webbrowser
 
 # Menampilkan logo
 col1, col2, col3 = st.columns([1, 1, 1])
@@ -7,15 +8,15 @@ with col2:
 
 # Data produk
 options_merchan = {
-    "Rak Dapur Pengering Piring": {"Harga": 120000, "gambar": "rakm.png"},
-    "Rak Organizer Susun": {"Harga": 90000, "gambar": "rakp.png"},
-    "Toples Penyimpanan": {"Harga": 75000, "gambar": "toples.png"},
-    "Sikat Electric": {"Harga": 150000, "gambar": "sikat.png"},
-    "Super Senter LED": {"Harga": 130000, "gambar": "senter.png"},
-    "Toothpick Otomatis": {"Harga": 85000, "gambar": "tootp.png"},
-    "Alat Pel Peras": {"Harga": 180000, "gambar": "pelperas.png"},
-    "Alat Pel Semprot": {"Harga": 200000, "gambar": "pelsemprot.png"},
-    "Vacuum Cleaner": {"Harga": 320000, "gambar": "vcm.png"}
+    "Rak Dapur Pengering Piring": {"page": st.Page(page="Produk page/rakp.py"), "gambar": "rakm.png"},
+    "Rak Organizer Susun": {"page": 90000, "gambar": "rakp.png"},
+    "Toples Penyimpanan": {"page": 75000, "gambar": "toples.png"},
+    "Sikat Electric": {"page": 150000, "gambar": "sikat.png"},
+    "Super Senter LED": {"page": 130000, "gambar": "senter.png"},
+    "Toothpick Otomatis": {"page": 85000, "gambar": "tootp.png"},
+    "Alat Pel Peras": {"page": 180000, "gambar": "pelperas.png"},
+    "Alat Pel Semprot": {"page": 200000, "gambar": "pelsemprot.png"},
+    "Vacuum Cleaner": {"page": 320000, "gambar": "vcm.png"}
 }
 
 index = 0
@@ -29,8 +30,14 @@ for row in range(3):
             with cols[col]:
                 st.image(details["gambar"], caption=option)
                 if st.button(f"Pesan {option} via Whatsapp"):
+                    webbrowser.open("https://wa.me/6282331257397?text=Permisi+kak,+saya+ingin+memesan+{selected_product}")
                     # Menampilkan link setelah tombol ditekan
-                    st.markdown(f'<a href="https://wa.me/6282331257397?text=Permisi+kak,+saya+ingin+memesan+{option}" target="_blank">Klik di sini</a>', unsafe_allow_html=True)
+                    #st.markdown(f'<a href="https://wa.me/6282331257397?text=Permisi+kak,+saya+ingin+memesan+{option}" target="_blank">Klik di sini</a>', unsafe_allow_html=True)
             index += 1
 
+col1, col2, col3 = st.columns([1, 10, 1])
+with col2:
+    st.markdown("<h3 style='text-align: center;'>Note:</h3>", unsafe_allow_html=True)
+    st.markdown("<p style='text-align: center;'>Mohon untuk melakukan video unboxing dan pengecekan barang</p>", unsafe_allow_html=True)
+    st.markdown("<p style='text-align: center;'>Komplain tanpa video unboxing dan foto bukti tidak akan di terima.</p>", unsafe_allow_html=True)
 
